0806dd3f265d3f614d9fabce80922655cfcd9a89
AbhiVeal <abiabira0204@gmail.com>
2024-01-28 21:05:17 +0530

    2nd update

---
+++ src/components/Home.js
@@ -0,0 +1,80 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <section>
+      <h2>Welcome to My Portfolio</h2>
+      <p>
+        Hi, I'm Abiramy Velavan, a passionate web developer. This is my portfolio where I showcase my projects and skills.
+      </p>
+    </section>
+
+     <section>
+      <h3>Studies</h3>
+      <ul>
+        <li>
+          <h4>On Going Degree at SLIIT in Information Technology [Bsc]</h4>
+        </li>
+      </ul>
+    </section>
+
+    <section>
+      <h3>Career</h3>
+      <ul>
+        <li>
+          <h4>Internship - as a front end developer [Reactjs] at Infonits.</h4>
+        </li>
+      </ul>
+    </section>
+
+    <section>
+      <h3>Skills</h3>
+      <ul>
+        <li>
+          <h4> Version Control</h4>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <h4>Analytical Thinking</h4>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <h4> Tailwind css / Bootstrap</h4>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <h4> Communication</h4>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <h4>  Creativity</h4>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <h4>  Time management</h4>
+        </li>
+      </ul>
+    </section>
+
+    <section>
+      <h3>Contact me</h3>
+      <ul>
+        <li>
+          <h4> Mobile No - 0773688981</h4>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <h4>  Email - abiabira0204@gmail.com</h4>
+        </li>
+      </ul>
+    </section>
+  </div>
+);
+
+export default Home;
